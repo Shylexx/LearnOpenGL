e75a5058cd1ceb48247bec08c8420168eaa4c647
Alex Dauncey <84759985+Shylexx@users.noreply.github.com>
2022-07-04 16:57:32 +0100

    A rectangle.

--- src/main.cpp
+++ src/main.cpp
@@ -68,9 +68,18 @@ int main()
 
 	// Triangle Vertex Data
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f};
+		0.5f, 0.5f, 0.0f,	// top right
+		0.5f, -0.5f, 0.0f,	// bottom right
+		-0.5f, -0.5f, 0.0f, // bottom left
+		-0.5f, 0.5f, 0.0f	// top left
+	};
+
+	// Indices for drawing a rect from two tris
+	unsigned int indices[] = {
+		// note that we start from 0!
+		0, 1, 3, // first Triangle
+		1, 2, 3	 // second Triangle
+	};
 
 	// Vertex Shader declared with an ID
 	unsigned int vertexShader;
@@ -145,6 +154,10 @@ int main()
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
 
+	// Element Buffer Object
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
 	// Bind Vertex Array First
 	glBindVertexArray(VAO);
 
@@ -161,6 +174,10 @@ int main()
 	// GL_STATIC_DRAW: the data is set only once and used many times.
 	// GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Configure EBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// Tell OpenGL how to interpret Vertex Data
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
@@ -194,8 +211,9 @@ int main()
 		// draw our first triangle
 		// Use the created shader program
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO); // seeing as we only have a single VsAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		// glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// Swap Color Buffers
 		glfwSwapBuffers(window);
